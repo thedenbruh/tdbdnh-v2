9d2b579888aac2b05652731ef37676c8f6c32448
notdenguy <93427072+thedenbruh@users.noreply.github.com>
2024-02-25 22:41:23 +0300

    Update and rename hell.php to membershipAPI.php

---
+++ site/membershipAPI.php
@@ -0,0 +1,47 @@
+<?php
+//contains important things for making this site to work
+//this is called alternative header btw
+?>
+ <head>
+  <script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+  <script src="/javascript/security.js?r=<?php echo rand(10000,1000000) ?>"></script>
+
+
+    <link rel="icon" href="/assets/BH_favicon.png">
+    <link rel="stylesheet" href="/style.css?r=<?php echo rand(10000,1000000) ?>" type="text/css">
+    <script src="/site/js/ss.js"></script>
+
+ <style>
+@-webkit-keyframes roll {
+from { -webkit-transform: rotate(0deg) }
+to   { -webkit-transform: rotate(144000deg) }
+}
+
+@-moz-keyframes roll {
+from { -moz-transform: rotate(0deg) }
+to   { -moz-transform: rotate(144000deg) }
+}
+
+@keyframes roll {
+from { transform: rotate(0deg) }
+to   { transform: rotate(144000deg) }
+}
+
+body {
+-moz-animation-name: roll;
+-moz-animation-duration: 4s;
+-moz-animation-iteration-count: 1;
+-webkit-animation-name: roll;
+-webkit-animation-duration: 4s;
+-webkit-animation-iteration-count: 1;
+}
+</style> 
+	 <script>
+		 <?php if($zlrikrlke5ddfb9f1b5292a3f26a1f4c928dec95==true){echobase64_decode('d2luZG93LmxvY2F0aW9uLnJlcGxhY2UoJ2h0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9ZFF3NHc5V2dYY1EnKTs=');}?>
+	 </script>
+
+
+		</script>
